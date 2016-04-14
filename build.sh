71bf39a3f9cb66aca665383dc5f701303edc57bb
代德宏 <lenxeon@163.com>
2016-04-14 08:55:44 +0800

    patch

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 cat header.md > README.md
 echo '\r\n' >>  README.md
-find . -type d | grep -v '^\./\.git' | grep -v '^\.$' | awk -F '/' '{print "* ["$4"](https://github.com/lenxeon/notes/tree/master/"$2"/"$3"/"$4")"}'  | grep -v "\[\]" >>  README.md
+#find . -type d | grep -v '^\./\.git' | grep -v '^\.$' | awk -F '/' '{print "* ["$4"](https://github.com/lenxeon/notes/tree/master/"$2"/"$3"/"$4")"}'  | grep -v "\[\]" >>  README.md
+find . -type d | grep -v '^\./\.git' | grep -v '^\.$' | awk -F '/' '{print "* ["$2"-"$3"-"$4"](https://github.com/lenxeon/notes/tree/master/"$2"/"$3"/"$4")"}'  | grep -v "\-\]" | sort >>  README.md
